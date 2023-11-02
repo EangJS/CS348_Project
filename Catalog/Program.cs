2523ed12eb18a1ea73f3f56879a0dabe84e1d4af
Eugene Ang <eugeneang654871@gmail.com>
2023-11-02 18:00:52 -0400

    Update program.cs

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -37,11 +37,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 app.UseCors(myCors);
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
